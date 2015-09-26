dfca19f3b60441bbb1affeebd90f5cce53498a6f
Anirudh Jayakumar <jayakumar.anirudh@gmail.com>
2015-09-25 23:48:10 -0500

    new files

---
+++ distMembership/src/main/java/edu/uiuc/cs425/Introducer.java
@@ -0,0 +1,22 @@
+package edu.uiuc.cs425;
+
+import java.nio.ByteBuffer;
+
+import org.apache.thrift.TException;
+
+import edu.uiuc.cs425.MemberIntroducer.Iface;
+
+public class Introducer implements Iface {
+
+
+	public int JoinGroup() throws TException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ByteBuffer GetMemebershipList() throws TException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
