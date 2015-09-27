00788d1a935f5912e40012f0b708581985e057ed
Banu Muthukumar <banu27@gmail.com>
2015-09-27 15:55:25 -0500

    Protobuf read and write started

--- distMembership/src/main/java/edu/uiuc/cs425/Membership.java
+++ distMembership/src/main/java/edu/uiuc/cs425/Membership.java
@@ -1,18 +1,59 @@
 package edu.uiuc.cs425;
 
 import java.nio.ByteBuffer;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import edu.uiuc.cs425.MembershipList.MemberList;
 
 public class Membership {
 	
-	private MembershipList m_oMembershipList;
+	//private MembershipList m_oMembershipList;
 	private int m_nTfail;
+	
+	private MemberList.Member AddMember(String nodeId, int heartbeatCounter, int localTime)
+	{
+		MemberList.Member.Builder memberList = MemberList.Member.newBuilder().setNodeId(nodeId);
+		memberList.setHeartbeatCounter(heartbeatCounter);
+		memberList.setLocalTime(localTime);
+	    
+	    return memberList.build();
+	 }
+	
+
+	private void AddToFile(String fileName, String nodeId, int heartbeatCounter, int localTime)
+	{
+		MemberList.Builder memberList = MemberList.newBuilder();
+
+	    // Read the existing address book.
+	    try {
+	      memberList.mergeFrom(new FileInputStream(fileName));
+	    } catch (FileNotFoundException e) {
+	      System.out.println(fileName + ": File not found.  Creating a new file.");
+	    }
+
+	    // Add an address.
+	    memberList.addMemberList(AddMember(nodeId, heartbeatCounter, localTime));
+
+	    // Write the new address book back to disk.
+	    FileOutputStream output = new FileOutputStream(fileName);
+	    memberList.build().writeTo(output);
+	    output.close();
+	}
+	
+
 	
 	public ByteBuffer GetMemeberList() {
 		//return m_oMembershipList.getByteBuffer();
 		return null;
 	}
 	
-	int MergeList(MembershipList incomingList)
+	/*int MergeList(MembershipList incomingList)
 	{
 		for(int i=0; i<incomingList.length(); i++)
 		{
@@ -25,7 +66,7 @@ public class Membership {
 		
 		
 	}
-	
+	*/
 	void DetectFailure(String nodeId) // will be called from thread as of now.
 	{
 		//Keep checking for Tcleanup = 2*Tfail secs and then do a detect.
@@ -41,5 +82,5 @@ public class Membership {
 		return Commons.SUCCESS;
 	}
 	
-	MembershipList m;
+	
 }
