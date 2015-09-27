eacf87487136c45303996941a67b7d9b08a0edbc
Banu Muthukumar <banu27@gmail.com>
2015-09-27 00:18:25 -0500

    Introducer done, membership started

--- distMembership/src/main/java/edu/uiuc/cs425/Introducer.java
+++ distMembership/src/main/java/edu/uiuc/cs425/Introducer.java
@@ -6,17 +6,24 @@ import org.apache.thrift.TException;
 
 import edu.uiuc.cs425.MemberIntroducer.Iface;
 
-public class Introducer implements Iface {
+public class Introducer implements Iface { //Why implements Iface??
 
-
+	private int m_nSerialNumber = 0;
+	private Membership m_oMembershipObject; //The membership object of the introducer
+	
 	public int JoinGroup() throws TException {
 		// TODO Auto-generated method stub
-		return 0;
+		//No threading so no lock
+		
+		m_nSerialNumber = m_nSerialNumber + 1;
+		return m_nSerialNumber;
 	}
 
 	public ByteBuffer GetMemebershipList() throws TException {
 		// TODO Auto-generated method stub
-		return null;
+		
+		return m_oMembershipObject.GetMemeberList();
+		
 	}
 
 }
--- distMembership/src/main/java/edu/uiuc/cs425/Membership.java
+++ distMembership/src/main/java/edu/uiuc/cs425/Membership.java
@@ -3,9 +3,37 @@ package edu.uiuc.cs425;
 import java.nio.ByteBuffer;
 
 public class Membership {
+	
+	private MembershipList m_oMembershipList;
+	private int m_nTfail;
+	
 	public ByteBuffer GetMemeberList() {
+		//return m_oMembershipList.getByteBuffer();
 		return null;
 	}
 	
-	MembershipList m;
+	int MergeList(MembershipList incomingList)
+	{
+		for(int i=0; i<incomingList.length(); i++)
+		{
+			//Find the same id in our list copy
+			//Update heartbeat counter
+				//If heartbeat counter hasnt changed, check local timestamp
+				//If suspected, call DetectFailure on this id alone
+			//Update the timestamp
+		}
+		
+		
+	}
+	
+	void DetectFailure(String nodeId) // will be called from thread as of now.
+	{
+		//Keep checking for Tcleanup = 2*Tfail secs and then do a detect.
+		
+	}
+
+	
+	
+	
+	
 }
