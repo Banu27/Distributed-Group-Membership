4a38408e98ed960494f4dff88fd6977a19b668b3
Banu Muthukumar <banu27@gmail.com>
2015-09-29 16:57:57 -0500

    Membership.java added byte array and mergeList completed

--- distMembership/src/main/java/edu/uiuc/cs425/Membership.java
+++ distMembership/src/main/java/edu/uiuc/cs425/Membership.java
@@ -1,6 +1,8 @@
 package edu.uiuc.cs425;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,128 +17,102 @@ import edu.uiuc.cs425.MembershipList.MemberList;
 
 public class Membership implements Runnable{
 	
-	private MembershipList 					m_oMembershipList;
+	private List<MembershipListStruct> 	    m_oMembershipListStructArray;
 	private int 							m_nTfail;
-	private MemberList.Builder 				m_oMembershipListBuilder;
 	private Thread	 						m_oSuspectedNodeThread;
-	//private ByteBuffer m_oMembershipListBuffer;
-	//private String m_sFileName;
 	
 	public void InitializeMemberList()
 	{
-		m_oMembershipListBuilder = MemberList.newBuilder();
+		m_oMembershipListStructArray = new ArrayList<MembershipListStruct>();
 	}
 	
-	public void AddMember(String nodeId, int heartbeatCounter, int localTime)
+	public void AddMemberToStruct(String nodeId, int heartbeatCounter, int localTime)
 	{
-		  
-		Member.Builder member = Member.newBuilder();
-		member.setNodeId(nodeId);
-		member.setHeartbeatCounter(heartbeatCounter);
-		member.setLocalTime(localTime);
-		m_oMembershipListBuilder.addMemberList(member.build());
-		m_oMembershipList.build();
-	 }
-	
-
-	/*public void UpdateMemberList(String nodeId, int heartbeatCounter, int localTime)
-	{
-		/*MemberList.Builder memberList = MemberList.newBuilder();
-
-	    // Read the existing address book.
-	    try {
-	      memberList.mergeFrom(new FileInputStream(fileName));
-	    } catch (FileNotFoundException e) {
-	      System.out.println(fileName + ": File not found.  Creating a new file.");
-	    }
-
-	    // Add a member
-	    m_oMembershipListBuilder.addMemberList(AddMember(nodeId, heartbeatCounter, localTime));
-
-	    // Write the new member list back to disk.
-	    ///FileOutputStream output = new FileOutputStream(m_sFileName);
-	    m_oMembershipList.build();
-	    //.writeTo(output);
-	    //output.close();
-	}*/
-	
-	public ByteBuffer GetMemeberList() {
-		//return m_oMembershipList.getByteBuffer();
-		return objectToByteBuffer(m_oMembershipList);
+		m_oMembershipListStructArray.add(new MembershipListStruct(nodeId, heartbeatCounter, localTime));
 	}
 	
-	public void ReceiveMembershipList(ByteBuffer incomingListBuffer)
+	public MembershipList CreateObject()
+	{	 
+		MemberList.Builder memberListBuilder  = MemberList.newBuilder();
+		List<Member.Buider> memberBuilderList = new ArrayList<Member.Builder>();
+		for(int i=0; i< m_oMembershipListStructArray.size(); i++)
+		{
+			Member.Builder member = Member.newBuilder();
+			member.setNodeId(m_oMembershipListStructArray.get(i).GetNodeId());
+			member.setHeartbeatCounter(m_oMembershipListStructArray.get(i).GetHeartbeatCounter());
+			member.setLocalTime(m_oMembershipListStructArray.get(i).GetLocalTime());
+			memberBuilderList.add(member.build);
+		}
+		memberListBuilder.addAll(memberBuilderList);
+		return memberListBuilder.build();
+	}
+		
+	public byte [] GetMemeberList() 
+	{	
+		return objectToByteBuffer(CreateObject());
+	}
+	
+	public void ReceiveMembershipList(byte [] incomingListBuffer)
 	{
-		MembershipList incomingList = objectFromByteBuffer(incomingListBuffer);
+		MembershipList incomingList = ObjectFromByteBuffer(incomingListBuffer);
 	}
 	
 	int MergeList(MembershipList incomingList)
 	{
 		//MemberList memberList = MemberList.parseFrom(new FileInputStream(fileName));
-		for(Member member : m_oMembershipList.getMemberList())
+		for(Member member : incomingList.getMemberList())
 		{ 
-			//How do I search??
-			if(member.getHeartbeatCounter() == incomingListMember.getHeartbeatCounter())
+			boolean found = false;
+			for(int i=0; i< m_oMembershipListStructArray.size(); i++)
 			{
-				if(member.getLocalTime() > m_nTfail)
+				if(member.getNodeId() == m_oMembershipListStructArray.get(i).GetNodeId())
 				{
-					DetectFailure(member.getNodeId());
+					found = true;
+					MembershipListStruct matchedMember = m_oMembershipListStructArray.get(i);
+					if(member.getHeartbeatCounter() == matchedMember.GetHeartbeatCounter())
+					{
+						if(matchedMember.GetLocalTime() > m_nTfail)
+						{
+							DetectFailure(matchedMember.GetNodeId());
+						}
+					}
+					else
+					{
+						matchedMember.ResetHeartbeatCounter(member.getHeatbeatCounter());
+					}
 				}
+				break;
 			}
-			else
+			if (found == false)
 			{
-				member.setHeartbeatCounter(incomingListMember.getHeartbeatCounter());
+				String nodeId = member.getNodeId();
+				int heartbeatCounter = member.getHeartbeatCounter();
+				int localTime = GetLocalTime(); //Our machine localTime
+				AddMemberToStruct(nodeId, heartbeatCounter, localTime);
 			}
 			
-			//Find the same id in our list copy
-			//Update heartbeat counter
-				//If heartbeat counter hasnt changed, check local timestamp
-				//If suspected, call DetectFailure on this id alone
-			//Update the timestamp
-		}		
-		
+		}	
+	}
+
+	public int GetLocalTime()
+	{
+		//DO
 	}
 	
-	//public byte[] objectToByteBuffer(Object o) throws Exception {
-	public ByteBuffer objectToByteBuffer(Object o) throws Exception {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	    Message message = (Message) o;
-	    byte[] name = message.getDescriptorForType().getFullName()
-	       .getBytes("UTF-8");
-	    baos.write(name.length); // TODO: Length as int and not byte
-	    
-	    // Write the full descriptor name, i.e. protobuf.Person
-	    baos.write(name);
-	    byte[] messageBytes = message.toByteArray();
-	    baos.write(messageBytes.length); // TODO: Length as int and not byte
-	    baos.write(messageBytes);
-	    return ByteBuffer.wrap(baos.toByteArray());
+	public byte[] ObjectToByteBuffer(Object o) throws Exception 
+	{
+		return o.toByteArray();
 	}
 	
-	public Object objectFromByteBuffer(byte[] buffer) throws Exception {
-	    ByteArrayInputStream bais = new ByteArrayInputStream(buffer);
-	    byte[] name = new byte[bais.read()];
-	    bais.read(name); // TODO: Read fully??
-	    // Get the class name associated with the descriptor name
-	    String className = mapping.get(new String(name, "UTF-8"));
-	    Class clazz = Thread.currentThread().getContextClassLoader()
-	       .loadClass(className);
-	    Method parseFromMethod = clazz.getMethod("parseFrom", byte[].class);
-	    byte[] message = new byte[bais.read()];
-	    bais.read(message); // TODO: Read fully??
-	    return parseFromMethod.invoke(null, message);
+	public Object ObjectFromByteBuffer(byte[] buffer) throws Exception 
+	{
+		return MembershipList,parseFrom(buffer);   //Need to make sure the message is the currect return type
 	 }
 	
+	//WHAT IS THIS FUNCTION DOING??
 	private bool sendMembershipList()
 	{
-		ByteBuffer eventTypeBuffer = ByteBuffer.allocate(1);
-		eventTypeBuffer.put(0x1c);
-		eventTypeBuffer.flip();
-		ByteString eventType = ByteString.copyFrom(eventTypeBuffer);
-		System.out.println(eventType.size() + " " + eventTypeBuffer.array().length);
-
-		Header.Builder mh = Header.newBuilder();
-		mh.setEventType(eventType);
+		return ObjectToByteBuffer(/*Object!*/);	
 	}
 	
 	void DetectFailure(String nodeId) // will be called from thread as of now.
@@ -147,7 +123,7 @@ public class Membership implements Runnable{
 	
 	void run()
 	{
-		
+		//Wait and recheck hearbeat counter
 	}
 
 	
