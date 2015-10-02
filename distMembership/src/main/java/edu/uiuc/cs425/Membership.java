a5339142bd5f17bbeb91a8e44bd2a28d965792a4
Banu Muthukumar <banu27@gmail.com>
2015-10-01 21:36:00 -0500

    Locks included

--- distMembership/src/main/java/edu/uiuc/cs425/Membership.java
+++ distMembership/src/main/java/edu/uiuc/cs425/Membership.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -32,29 +34,39 @@ public class Membership implements Runnable{
 	private int 										m_nSerialNumber;
 	private String 										m_nIP;
 	private int 										m_nMyHeartBeat;
+	private ReentrantReadWriteLock 				m_oReadWriteLock; 
+	private Lock 									m_oLockR; 
+	private Lock 									m_oLockW; 
+	
 	
 	public void Initialize()
 	{
 		m_oHmap 		= new HashMap<Integer, MembershipListStruct>();
 		m_nMyHeartBeat  = 1;
+		m_oReadWriteLock = new ReentrantReadWriteLock();
+		m_oLockR = m_oReadWriteLock.readLock();
+		m_oLockW = m_oReadWriteLock.writeLock();
+		
 		try {
 			m_nIP  = InetAddress.getLocalHost().getHostAddress();
 		} catch (UnknownHostException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-					
-		//DetectFailure();
 	}
 	
 	public void AddSelf(int serialNumber)
 	{
+		//Write lock. Add self called from controlller
+		m_oLockW.lock();
 		m_nSerialNumber = serialNumber;
 		AddMemberToStruct(m_nIP, m_nMyHeartBeat, GetMyLocalTime(), m_nSerialNumber);
+		m_oLockW.unlock();
 	}
 	
 	public void AddMemberToStruct(String IP, int heartbeatCounter, long localTime, int serialNumber)
 	{
+		//No write lock. Write lock present in Merge.
 		MembershipListStruct newMember = new MembershipListStruct(IP, heartbeatCounter, localTime, serialNumber);
 		m_oHmap.put(serialNumber,newMember);
 	}
@@ -66,6 +78,8 @@ public class Membership implements Runnable{
 	
 	public MemberList CreateObject()
 	{	 
+		//Read lock
+		m_oLockR.lock();
 		MemberList.Builder memberListBuilder  = MemberList.newBuilder();
 		List<Member> memberList = new ArrayList<Member>();
 		Set<Entry<Integer, MembershipListStruct>> set = m_oHmap.entrySet();
@@ -81,7 +95,9 @@ public class Membership implements Runnable{
 	         memberList.add(member.build());
 	    }	      
 		memberListBuilder.addAllMember(memberList);
+		m_oLockR.unlock();
 		return memberListBuilder.build();
+		
 	}
 		
 	public byte [] GetMemberList() throws Exception 
@@ -92,6 +108,9 @@ public class Membership implements Runnable{
 	public int MergeList(byte [] incomingListBuffer) throws Exception
 	{
 		MemberList incomingList = ObjectFromByteBuffer(incomingListBuffer);
+		
+		//Write lock 
+		m_oLockW.lock();
 		for(Member member : incomingList.getMemberList())
 		{ 
 			boolean found = false;
@@ -120,6 +139,7 @@ public class Membership implements Runnable{
 				AddMemberToStruct(IP, heartbeatCounter, localTime, serialNumber);
 			}
 		}
+		m_oLockW.unlock();
 		PrintList();
 		return Commons.SUCCESS;
 	}
@@ -172,8 +192,12 @@ public class Membership implements Runnable{
 				
 				e.printStackTrace();
 			}
+			
 			Set<Entry<Integer, MembershipListStruct>> set = m_oHmap.entrySet();
 			Iterator<Entry<Integer, MembershipListStruct>> iterator = set.iterator();
+			//No need for write lock. No other thread or function can set suspect. 
+			//Read lock imposed due to local time read
+			m_oLockR.lock();
 			while(iterator.hasNext()) {
 				Map.Entry mentry = (Map.Entry)iterator.next();
 				MembershipListStruct memberStruct = m_oHmap.get(mentry.getKey());
@@ -192,9 +216,11 @@ public class Membership implements Runnable{
 					}
 				}
 			}
+			m_oLockR.unlock();
 		}
 	}
 
+	//Read lock
 	 public ArrayList<Integer> GetMemberIds() 
 	 {
 		 return new ArrayList<Integer>(m_oHmap.keySet());
